834de8a46c2fe22eca2727740d99371b749c3e15
superoverflow <me@superoverflow.com>
2022-11-10 00:58:54 +0000

    adding form submission

---
+++ pages/api/submit.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import {google} from "googleapis";
+import { SheetForm } from "../rsvp";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    const body = req.body as SheetForm
+
+    try {
+        const auth = new google.auth.GoogleAuth({
+            credentials: {
+                client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+            },
+            scopes: [
+                'https://www.googleapis.com/auth/drive',
+                'https://www.googleapis.com/auth/drive.file',
+                'https://www.googleapis.com/auth/spreadsheets'
+            ]
+        })
+
+        const sheets = google.sheets({
+            auth,
+            version: 'v4'
+        });
+        
+        const response = await sheets.spreadsheets.values.append({
+            spreadsheetId: process.env.GOOGLE_SHEET_ID,
+            range: 'A1:D1',
+            valueInputOption: 'USER_ENTERED',
+            requestBody: {
+                values: [
+                    [body.name, body.attend, body.guests, body.message]
+                ]
+            }
+        });
+
+        return res.status(201).json({
+            data: response.data
+        })
+    } catch (e: any) {
+        return res.status(e.code).send({message: e.message})
+    }
+
+}
---
+++ pages/rsvp.tsx
@@ -0,0 +1,92 @@
+import type { NextPage } from "next";
+import {
+  TextInput,
+  Textarea,
+  Checkbox,
+  Button,
+  NumberInput,
+  Group,
+  Center,
+  LoadingOverlay,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { useMutation } from "react-query";
+
+export type SheetForm = {
+  name: string;
+  attend: boolean;
+  guests: number;
+  message: string;
+};
+
+const handleSubmit = async (values: SheetForm) => {
+  const rawResponse = await fetch("/api/submit", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(values),
+  });
+  const content = await rawResponse.json();
+  console.log({ content });
+};
+
+const Rsvp: NextPage = () => {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      attend: true,
+      guests: 1,
+      message: "",
+    },
+
+    validate: {
+      name: (value) => (value ? null : "點稱呼？"),
+    },
+  });
+
+  const submitForm = useMutation({
+    mutationFn: (values: SheetForm) => handleSubmit(values),
+  });
+
+  return (
+    <Center sx={{ maxWidth: 300 }} mx="auto">
+      <LoadingOverlay visible={submitForm.isLoading} overlayBlur={2} />
+      <form onSubmit={form.onSubmit((values) => submitForm.mutate(values))}>
+        <TextInput
+          withAsterisk
+          label="Name"
+          placeholder="高姓大名"
+          {...form.getInputProps("name")}
+        />
+
+        <Checkbox
+          mt="md"
+          label="我會出席！"
+          {...form.getInputProps("attend", { type: "checkbox" })}
+        />
+
+        <NumberInput
+          label="No. of guests"
+          placeholder="幾位？"
+          {...form.getInputProps("guests")}
+        />
+
+        <Textarea
+          label="message"
+          placeholder="仲有乜想講？"
+          minRows={4}
+          {...form.getInputProps("message")}
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Thank you!</Button>
+        </Group>
+        {submitForm.isSuccess ? <div>Got it!</div> : null}
+      </form>
+    </Center>
+  );
+};
+
+export default Rsvp;
