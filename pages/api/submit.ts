5ba64421f34a0b00f442fee66721f5b32f016486
superoverflow <me@superoverflow.com>
2022-11-17 21:44:18 +0000

    Fix module name

--- pages/api/submit.ts
+++ pages/api/submit.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import {google} from "googleapis";
-import { SheetForm } from "../../components/Rsvp";
+import { SheetForm } from "../../components/rsvp";
 
 export default async function handler(
     req: NextApiRequest,
