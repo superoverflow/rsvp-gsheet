9e362e567cc41797bf4ffdf68efa9f1e125bd7c2
superoverflow <me@superoverflow.com>
2022-11-19 10:38:58 +0000

    Add favico

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,19 +7,15 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Tiffany & Cyrus | 24/01/23</title>
+        <title>RSVP</title>
         <meta property="og:title" content="You are invited! 24 Jan 2023" />
         <meta property="og:image" content="og.jpg" />
-
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👰🏻‍♀️</text></svg>"
-        />
+        <link rel="icon" href="/wedding.png" />
       </Head>
 
-        <Envelope>
-          <Information />
-        </Envelope>
+      <Envelope>
+        <Information />
+      </Envelope>
     </>
   );
 };
--- pages/rsvp.tsx
+++ pages/rsvp.tsx
@@ -10,11 +10,7 @@ const Rsvp: NextPage = () => {
         <title>Tiffany & Cyrus | 24/01/23</title>
         <meta property="og:title" content="You are invited! 24 Jan 2023" />
         <meta property="og:image" content="og.jpg" />
-
-        <link
-          rel="icon"
-          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👰🏻‍♀️</text></svg>"
-        />
+        <link rel="icon" href="/wedding.png" />
       </Head>
       <Flex
         style={{
