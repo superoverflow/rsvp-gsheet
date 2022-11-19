5c650006efe2f30e3761e9f8e1f50aed2314b027
superoverflow <me@superoverflow.com>
2022-11-19 21:09:55 +0000

    update date

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Tiffany & Cyrus | 24/01/23</title>
+        <title>Tiffany & Cyrus | 24/01/2023</title>
         <meta property="og:title" content="You are invited! 24 Jan 2023" />
         <meta property="og:image" content="og.jpg" />
         <link rel="icon" href="/wedding.png" />
